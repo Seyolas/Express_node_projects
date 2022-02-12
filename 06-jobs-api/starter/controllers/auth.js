f045af7d24a19e71c7573f79d08f1703a0e18cb2
Seydi Sarıtaş <saritas.seydi@gmail.com>
2022-02-12 15:38:48 +0300

    jwt

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,17 +1,29 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const {BadRequestError} = require("../errors")
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
 
+  // YOU CAN DO THE LOGIC IN HERE LIKE THAT
+  
   // const {name,email,password} = req.body;
   // if (!name || !email || !password) {
   //   throw new BadRequestError('Please provide name email password')
   // }
-  console.log(req.body);
-  const user = await User.create(req.body)
- 
-  res.status(StatusCodes.CREATED).json(user)
+
+
+  // YOU CAN HASH THE PASSWORD LIKE THAT
+  // const {name,email,password} = req.body;
+  // const salt = await bcrypt.genSalt(5);
+  // const hashedPassword = await bcrypt.hash(password,salt)
+  // const tempUser = {name,email,password:hashedPassword}
+
+  const user = await User.create({...req.body})
+  // const token = jwt.sign({userId:user._id,name:user.name},'jwtSecret',{expiresIn:'30d'})
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({user:{name:user.name},token})
+
 }
 
 const login = async (req, res) => {
--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,5 +1,6 @@
-
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -22,5 +23,15 @@ const UserSchema = new mongoose.Schema({
         minlength: 6,
       },
 })
+
+UserSchema.pre('save',async function(){
+  const salt = await bcrypt.genSalt(5)
+  this.password = await bcrypt.hash(this.password,salt)
+})
+
+UserSchema.methods.createJWT = function (){
+   return jwt.sign({userId:user._id,name:user.name},'jwtSecret',{expiresIn:'30d'})
+    
+}
 
 module.exports = mongoose.model('User', UserSchema)
